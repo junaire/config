98787882de5026b6ba57a7ebcbe98cbe17b91dae
Jun <jun@junz.org>
2021-09-02 22:15:35 +0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #! /bin/bzsh
 
-ln -s nvim/init.vim ~/.config/nvim/init.vim
-ln -s nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+mkdir -p ~/.config/nvim
+ln -s ~/config/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
 
-ln -s git/gitconfig ~/.gitconfig
+ln -s ~/config/git/gitconfig ~/.gitconfig
 
-ln -s tmux/tmux.conf ~/.tmux.conf
+ln -s ~/config/tmux/tmux.conf ~/.tmux.conf
 
