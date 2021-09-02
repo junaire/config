bb282c3ac7f2e33cbce8a80bc2e49da87230d2fd
Jun <jun@junz.org>
2021-09-02 21:57:38 +0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#! /bin/bzsh
+
+ln -s nvim/init.vim ~/.config/nvim/init.vim
+ln -s nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+
+ln -s git/gitconfig ~/.gitconfig
+
+ln -s tmux/tmux.conf ~/.tmux.conf
+
