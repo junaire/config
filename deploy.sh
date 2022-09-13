e00037d8c91e6b21dcb34b6a8c95922a45ea9279
Jun Zhang <jun@junz.org>
2022-09-13 10:31:38 +0800

    Use context non-sensitive dir in deploy.sh
    Signed-off-by: Jun Zhang <jun@junz.org>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# Remove everything
 rm -rf ${HOME}/.gitconfig
 rm -rf ${HOME}/.tmux.conf
 rm -rf ${HOME}/.gdbinit
@@ -8,29 +9,32 @@ rm -rf ${HOME}/.config/nvim
 rm -rf ${HOME}/.config/pip
 rm -rf ${HOME}/.config/wezterm
 
+# Where we put our config.
+CONF_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 # git
-ln -s ~/config/git/gitconfig  ${HOME}/.gitconfig
+ln -s ${CONF_DIR}/git/gitconfig ${HOME}/.gitconfig
 # tmux
-ln -s ~/config/tmux/tmux.conf ${HOME}/.tmux.conf
+ln -s ${CONF_DIR}/tmux/tmux.conf ${HOME}/.tmux.conf
 # gdb
-ln -s ~/config/gdb/gdbinit    ${HOME}/.gdbinit
+ln -s ${CONF_DIR}/gdb/gdbinit ${HOME}/.gdbinit
 
 # zsh
-echo "source ~/config/zsh/zshrc" > ${HOME}/.zshrc
+echo "source ${CONF_DIR}/zsh/zshrc" > ${HOME}/.zshrc
 
 # ssh config
 if [ ! -d "${HOME}/.ssh" ]; then
     mkdir ${HOME}/.ssh
 fi
-ln -s ~/config/ssh/config ${HOME}/.ssh/config
+ln -s ${CONF_DIR}/ssh/config ${HOME}/.ssh/config
 
 if [ ! -d "${HOME}/.config" ]; then
     mkdir ${HOME}/.config
 fi
 
 # neovim
-ln -s ~/config/nvim ${HOME}/.config/nvim
+ln -s ${CONF_DIR}/nvim ${HOME}/.config/nvim
 # pip
-ln -s ~/config/pip ${HOME}/.config/pip
+ln -s ${CONF_DIR}/pip ${HOME}/.config/pip
 # wezterm
-ln -s ~/config/wezterm ${HOME}/.config/wezterm
+ln -s ${CONF_DIR}/wezterm ${HOME}/.config/wezterm
