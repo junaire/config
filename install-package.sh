e5b1a53b072ab1fb11335d15c548e53577faeebc
Jun Zhang <jun@junz.org>
2022-11-25 16:20:50 +0800

    Make it optional to install fonts
    Signed-off-by: Jun Zhang <jun@junz.org>

--- install-package.sh
+++ install-package.sh
@@ -6,33 +6,19 @@ NEOVIM_NIGHTLY="https://github.com/neovim/neovim/releases/download/nightly/nvim-
 WEZTERM_LOC="/tmp/wezterm-nightly.deb"
 NEOVIM_LOC="/tmp/neovim-nightly.deb"
 
-# update the packages every month.
-is_need_update() {
-    day=$(date "+%d")
-    # If today is the first day of the month, let's try to do a update.
-    if [ ${day} -eq 1 ]
-    then
-	true
-    else
-	false
-    fi
-}
-
-
 # I love Jetbrains Mono!
 install_font() {
     # Ref: https://github.com/JetBrains/JetBrainsMono
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
 }
 
-install_packages() {
-    if is_need_update; then
-	install_font
-	wget ${WEZTERM_NIGHTLY} -O ${WEZTERM_LOC}
-	wget ${NEOVIM_NIGHTLY}  -O ${NEOVIM_LOC}
-	sudo dpkg -i ${WEZTERM_LOC}
-	sudo dpkg -i ${NEOVIM_LOC}
-    fi
-}
+if [[ "$1" == "--install-font" ]]; then
+    install_font
+fi
 
-install_packages
+wget ${WEZTERM_NIGHTLY} -O ${WEZTERM_LOC}
+wget ${NEOVIM_NIGHTLY}  -O ${NEOVIM_LOC}
+sudo dpkg -i ${WEZTERM_LOC}
+sudo dpkg -i ${NEOVIM_LOC}
+rm -rf WEZTERM_LOC
+rm -rf NEOVIM_LOC
