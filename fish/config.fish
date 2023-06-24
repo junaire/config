e4f15b1740c53572dfa3e91c9177d02ca84c1b60
Jun Zhang <jun@junz.org>
2023-06-25 00:04:49 +0800

    fish: more customization
    Signed-off-by: Jun Zhang <jun@junz.org>

--- fish/config.fish
+++ fish/config.fish
@@ -1,26 +1,27 @@
-
 # Fish git prompt
 set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_hide_untrackedfiles 1
 
 set -g __fish_git_prompt_color_branch green
 set -g __fish_git_prompt_showupstream "informative"
 set -g __fish_git_prompt_char_upstream_ahead "↑"
 set -g __fish_git_prompt_char_upstream_behind "↓"
 set -g __fish_git_prompt_char_upstream_prefix ""
+set -g __fish_git_prompt_color_upstream brgreen
 
-set -g __fish_git_prompt_char_stagedstate "●"
-set -g __fish_git_prompt_char_dirtystate "✚"
-set -g __fish_git_prompt_char_untrackedfiles "!"
+set -g __fish_git_prompt_char_stagedstate "+"
+set -g __fish_git_prompt_char_dirtystate "!"
+set -g __fish_git_prompt_char_untrackedfiles "?"
 set -g __fish_git_prompt_char_conflictedstate "✖"
 set -g __fish_git_prompt_char_cleanstate "✔"
 
-set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_dirtystate yellow
 set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_color_invalidstate red
-set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_untrackedfiles brblue
 set -g __fish_git_prompt_color_cleanstate green
 set -g __fish_prompt_git_status_order staged conflicted changed untracked
+
+set -g __fish_git_prompt_char_stateseparator ''
 
 # colored man output
 # from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
@@ -51,8 +52,9 @@ end
 function fish_prompt
     set_color brblue
     echo -n $PWD | sed -e "s|^$HOME|~|"
-    set_color green
+    set_color normal
     printf '%s' (__fish_git_prompt)
     echo ''
-    echo -n '$ '
+    echo -n '% '
+    set_color normal
 end
