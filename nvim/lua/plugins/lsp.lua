cb2faf4a63f3794993356d3001fbdb289ebd307f
Jun Zhang <jun@junz.org>
2024-07-29 17:51:44 +0800

    bump up
    Signed-off-by: Jun Zhang <jun@junz.org>

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -26,6 +26,22 @@ return {
         },
         servers = {
             pyright = {},
+            tsserver = {
+                settings = {
+                    typescript = {
+                        inlayHints = {
+                            includeInlayParameterNameHints = 'all',
+                            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                            includeInlayFunctionParameterTypeHints = true,
+                            includeInlayVariableTypeHints = true,
+                            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+                            includeInlayPropertyDeclarationTypeHints = true,
+                            includeInlayFunctionLikeReturnTypeHints = true,
+                            includeInlayEnumMemberValueHints = true,
+                        }
+                    },
+                }
+            },
             clangd = {
                 extensions = {
                     autoSetHints = false,
