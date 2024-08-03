6de57d50c658624935da3931c32a23a6c1ad0930
Jun Zhang <jun@junz.org>
2024-08-03 14:33:57 +0800

    update

--- nvim/lua/core/filetype.lua
+++ nvim/lua/core/filetype.lua
@@ -2,6 +2,8 @@ local cmd = vim.cmd
 local ncmd = vim.api.nvim_command
 
 ncmd('filetype plugin indent on')
-cmd([[ autocmd FileType c,cpp setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=80 expandtab ]])
-cmd([[ autocmd FileType solidity,lua,python setlocal tabstop=4 shiftwidth=4 softtabstop=4 textwidth=80 expandtab ]])
+cmd([[ autocmd FileType c,cpp setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab ]])
+cmd([[ autocmd FileType solidity,lua,python setlocal tabstop=4 shiftwidth=4 softtabstop=4 expandtab ]])
 cmd([[ autocmd FileType json setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab ]])
+cmd(
+    [[ autocmd FileType javascript,javascript.jsx,javascriptreact,typescript,typescriptreact setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab ]])
